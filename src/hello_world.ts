10ae85bbf5756e87f4797030c3ed607e200eafd0
albertvinicius10 <albertvinicius10@gmail.com>
2025-06-06 00:47:24 -0400

    teste

--- src/hello_world.ts
+++ src/hello_world.ts
@@ -3,7 +3,7 @@ const greeting = 'world';
 
 export function hello(world: string = greeting): string {
    if (world == "WebAcademy") {
-       return "Hi WebAcademy!";
+       return "Hi WebAcademy !";
    }
    return `Hello ${world}!`;
 }
